22f91669d4545d2811ec3dec26845e84d1417b6f
Roman Kuepper <rrkuepper@gmail.com>
2020-12-14 20:50:53 +0100

    some more desc + preps aggregates

--- Workbook_descriptive.R
+++ Workbook_descriptive.R
@@ -30,18 +30,16 @@ qplot(x = factor(year), y = Y_his,
       geom = "boxplot",
       data = tanzania)
 
-# stunting ratio over time
-ggplot(subset(tanzania, !is.na(tanzania$stunting)), aes(x=factor(year,levels=2005:2015))) +
-  geom_bar(aes(fill = stunting == 1), position = "fill", width = 0.4) + scale_color_grey() + theme_classic() +
-  labs(y = "proportion", fill ="stunting") +
-  scale_x_discrete('year', breaks=factor(2005:2015), drop=TRUE) + scale_fill_grey()
+# show how income has changed over time + rural vs urban
+qplot(x = factor(year), y = Y_his, colour=factor(urban),
+geom = "boxplot",
+data = tanzania)
 
-# dead5 ratio over time
-ggplot(subset(tanzania, !is.na(tanzania$dead5)), aes(x=factor(year,levels=2005:2015))) +
-  geom_bar(aes(fill = dead5 == 1), position = "fill", width = 0.4) + scale_color_grey() + theme_classic() +
-  labs(y = "proportion", fill ="dead5") +
-  scale_x_discrete('year', breaks=factor(2005:2015), drop=TRUE) + scale_fill_grey()
-  
+# show how income differs per region and time (for ANNEX)
+qplot(x = region, y = Y_his, colour=factor(year),
+      geom = "boxplot",
+      data = tanzania)
+
 # descriptive summary table - V1 (kein latex)
 library(table1)
 library(rvest)
@@ -50,6 +48,48 @@ table1::label(tanzania$stunting) <- "Life Expectancy"
 table1::label(tanzania$dead5) <- "Population"
 table1::label(tanzania$Y_his) <- "Gdp Per Capita"
 table1::table1(~stunting + dead5 + Y_his | year, data = tanzania)
+
+# stunting ratio over time
+ggplot(subset(tanzania, !is.na(tanzania$stunting)), aes(x=factor(year,levels=2005:2015))) +
+  geom_bar(aes(fill = stunting == 1), position = "fill", width = 0.4) + theme_classic() +
+  labs(y = "proportion", fill ="stunting") +
+  scale_x_discrete('year', breaks=factor(2005:2015), drop=TRUE)
+
+# dead5 ratio over time
+ggplot(subset(tanzania, !is.na(tanzania$dead5)), aes(x=factor(year,levels=2005:2015))) +
+  geom_bar(aes(fill = dead5 == 1), position = "fill", width = 0.4) + theme_classic() +
+  labs(y = "proportion", fill ="dead5") +
+  scale_x_discrete('year', breaks=factor(2005:2015), drop=TRUE)
+
+# water_improved_total ratio over time
+ggplot(subset(tanzania, !is.na(tanzania$water_improved_total)), aes(x=factor(year,levels=2005:2015))) +
+  geom_bar(aes(fill = water_improved_total == 1), position = "fill", width = 0.4) + theme_classic() +
+  labs(y = "proportion", fill ="water_improved_total") +
+  scale_x_discrete('year', breaks=factor(2005:2015), drop=TRUE)
+  
+# sanitation_improved_total ratio over time
+ggplot(subset(tanzania, !is.na(tanzania$sani_improved_total)), aes(x=factor(year,levels=2005:2015))) +
+  geom_bar(aes(fill = sani_improved_total == 1), position = "fill", width = 0.4) + theme_classic() +
+  labs(y = "proportion", fill ="sani_improved_total") +
+  scale_x_discrete('year', breaks=factor(2005:2015), drop=TRUE)
+
+
+# education ratio over time
+ggplot(subset(tanzania, !is.na(tanzania$education)), aes(x=factor(year,levels=2005:2015))) +
+  geom_bar(aes(fill = education), position = "fill", width = 0.4) + theme_classic() +
+  labs(y = "proportion", fill ="education") +
+  scale_x_discrete('year', breaks=factor(2005:2015), drop=TRUE)
+
+
+tanzania$agg_education <- ifelse(tanzania$mo_noedu == 1,"noedu",
+                         ifelse(tanzania$mo_secondary == 1, 'secondary',
+                                ifelse(tanzania$mo_primary ==1, 'primary', 'invalid')))
+
+
+
+
+
+
 
 
 
@@ -121,3 +161,13 @@ ggplot(tanzania,
 ggplot(tanzania, aes(x = year)) +
   geom_bar(aes(fill = dead5 == 1), position = position_stack(reverse = TRUE)) +
   theme(legend.position = "top")
+
+
+
+
+
+
+
+
+
+
